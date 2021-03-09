80f75a0a98c70aee98d4769b85f23e8521d9a05b
kaisei <khishida55@gmail.com>
2021-03-09 18:09:42 +0900

    Added physical movement

---
+++ Assets/scripts/MainCameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCameraController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Camera camera;
+    private Vector3 INITIALPOS;
+    void Start()
+    {
+        camera = GetComponent<Camera>();
+        //相対座標
+        INITIALPOS = camera.transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.LeftShift)) {
+            camera.transform.localPosition += new Vector3(0,0.001f,-0.001f);
+        } else {
+            camera.transform.localPosition = INITIALPOS;
+        }
+    }
+}
