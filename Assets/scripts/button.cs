8c531600c52bb11d8920315f6cdab7f810f74234
kaisei <khishida55@gmail.com>
2021-03-10 13:46:06 +0900

    buttonとcamera切り替えの追加

---
+++ Assets/scripts/button.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class button : MonoBehaviour
+{
+
+    public Camera FirstCamera;
+    public Camera SecondCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FirstCamera.enabled = true;
+        SecondCamera.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onClick() {
+        enabled = FirstCamera.enabled;
+        FirstCamera.enabled = SecondCamera.enabled;
+        SecondCamera.enabled = enabled;
+    }
+
+}
